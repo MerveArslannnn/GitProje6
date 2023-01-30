4f3ca0fdb12161260250b3f3dd961df6003dd76e
merve <mervegz263@gmail.com>
2023-01-30 19:22:59 +0300

    1

--- src/Gun01/Student.java
+++ src/Gun01/Student.java
@@ -4,6 +4,8 @@ public class Student {
     public static void main(String[] args) {
         System.out.println("Merhaba 1");
         System.out.println("Nasılsın");
+        String isim="Merve";
+        System.out.println("isim = " + isim);
 
 
 
